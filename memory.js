5cd02af78c963f53ae677af590bab534397ba7f5
frncsdrk <carlsmoh@gmail.com>
2018-08-18 20:45:07 +0200

    Use ES6 features

--- memory.js
+++ memory.js
@@ -1,20 +1,20 @@
 module.exports = function (RED) {
-  var exec = require('child_process').exec
+  const exec = require('child_process').exec
 
   function MemoryNode (conf) {
     RED.nodes.createNode(this, conf)
 
     this.name = conf.name
 
-    var node = this
+    const node = this
 
-    node.on('input', function (msg) {      
+    node.on('input', (msg) => {      
       exec('free -mt | grep "Total"', (err, stdout, stderr) => {
         if (err) {
           node.error('Error', err.message)
           return false
         }
-        var regex = /[0-9]+/g
+        const regex = /[0-9]+/g
         const matched = stdout.match(regex)
         node.send({
           payload: matched[1], // free

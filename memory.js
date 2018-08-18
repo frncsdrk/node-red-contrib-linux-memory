9928645281301d39b4bde563817fbe7f438e4524
frncsdrk <carlsmoh@gmail.com>
2018-08-18 16:14:04 +0200

    Add free memory output

---
+++ memory.js
@@ -0,0 +1,28 @@
+module.exports = function (RED) {
+  var exec = require('child_process').exec
+
+  function MemoryNode (conf) {
+    RED.nodes.createNode(this, conf)
+
+    this.name = conf.name
+
+    var node = this
+
+    node.on('input', function (msg) {      
+      exec('free -mt | grep "Total"', (err, stdout, stderr) => {
+        if (err) {
+          node.error('Error', err.message)
+          return false
+        }
+        var regex = /[0-9]+/g
+        const matched = stdout.match(regex)
+        node.send({
+          payload: matched[1], // free
+          topic: 'memory_free'
+        })
+      })
+    })
+  }
+
+  RED.nodes.registerType('memory', MemoryNode)
+}

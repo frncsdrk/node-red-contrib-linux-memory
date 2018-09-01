7b87f2600009e48ed32f2aed4f9b855eecb00181
frncsdrk <carlsmoh@gmail.com>
2018-09-01 11:45:41 +0200

    Add calculating of values including buffer and cache
    update readme
    refactoring
    clean up

--- README.md
+++ README.md
@@ -3,6 +3,20 @@ node-red-contrib-linux-memory
 
 > collect linux system memory stats
 
+## available values
+
+**excl. buffer / cache**
+- available
+- active
+- free available
+
+**incl. buffer / cache**
+- total
+- used
+- free
+
+One can decide if to use absolute or relative values.
+
 ## Copyright info
 
 Linux is a registered trademark of Linus Torvalds.
--- memory.js
+++ memory.js
@@ -11,9 +11,14 @@ module.exports = function (RED) {
 
     this.name = conf.name
     this.relativeValues = (typeof conf.relativeValues === 'undefined') ? false : conf.relativeValues
+
     this.availableMemory = (typeof conf.availableMemory === 'undefined') ? false : conf.availableMemory
     this.activeMemory = (typeof conf.activeMemory === 'undefined') ? true : conf.activeMemory
     this.freeAvailableMemory = (typeof conf.freeAvailableMemory === 'undefined') ? false : conf.freeAvailableMemory
+
+    this.totalMemory = (typeof conf.totalMemory === 'undefined') ? false : conf.totalMemory
+    this.usedMemory = (typeof conf.usedMemory === 'undefined') ? false : conf.usedMemory
+    this.freeMemory = (typeof conf.freeMemory === 'undefined') ? false : conf.freeMemory
 
     const node = this
 
@@ -36,46 +41,110 @@ module.exports = function (RED) {
   }
 
   MemoryNode.prototype.calculatePayloadsAbsolute = function (data, payloadArr) {
-    if (this.availableMemory) {
-      payloadArr.push({
-        payload: toMb(data.available), // total
-        topic: 'memory_available_mb'
-      })
-    }
-    if (this.activeMemory) {
-      payloadArr.push({
-        payload: toMb(data.active), // used
-        topic: 'memory_active_mb'
-      })
-    }
-    if (this.freeAvailableMemory) {
-      payloadArr.push({
-        payload: toMb(data.available - data.active), // free
-        topic: 'memory_free_available_mb'
-      })
+    const possiblePayloads = [
+      { // available memory
+        condition: this.availableMemory,
+        result: {
+          payload: toMb(data.available), // available
+          topic: 'memory_available_mb'
+        }
+      },
+      { // active memory
+        condition: this.activeMemory,
+        result: {
+          payload: toMb(data.active), // active
+          topic: 'memory_active_mb'
+        }
+      },
+      { // free available memory
+        condition: this.freeAvailableMemory,
+        result: {
+          payload: toMb(data.available - data.active), // free
+          topic: 'memory_free_available_mb'
+        }
+      },
+      { // total memory
+        condition: this.totalMemory,
+        result: {
+          payload: toMb(data.total), // total
+          topic: 'memory_total_mb'
+        }
+      },
+      { // used memory
+        condition: this.usedMemory,
+        result: {
+          payload: toMb(data.used), // used
+          topic: 'memory_used_mb'
+        }
+      },
+      { // free memory
+        condition: this.freeMemory,
+        result: {
+          payload: toMb(data.free), // free
+          topic: 'memory_free_mb'
+        }
+      }
+    ]
+    for (let i = 0; i < possiblePayloads.length; i++) {
+      const possiblePayloadsItem = possiblePayloads[i]
+      if (possiblePayloadsItem.condition) {
+        payloadArr.push(possiblePayloadsItem.result)
+      }
     }
 
     return payloadArr
   }
 
   MemoryNode.prototype.calculatePayloadsRelative = function (data, payloadArr) {
-    if (this.availableMemory) {
-      payloadArr.push({
-        payload: 100, // total
-        topic: 'memory_available_per_cent'
-      })
-    }
-    if (this.activeMemory) {
-      payloadArr.push({
-        payload: toMb(data.active) / (toMb(data.available) / 100), // active
-        topic: 'memory_active_per_cent'
-      })
-    }
-    if (this.freeAvailableMemory) {
-      payloadArr.push({
-        payload: toMb(data.available - data.active) / (toMb(data.available) / 100), // free excl. buffer / cache
-        topic: 'memory_active_free_per_cent'
-      })
+    const possiblePayloads = [
+      { // available memory
+        condition: this.availableMemory,
+        result: {
+          payload: 100, // available
+          topic: 'memory_available_per_cent'
+        }
+      },
+      { // active memory
+        condition: this.activeMemory,
+        result: {
+          payload: toMb(data.active) / (toMb(data.available) / 100), // active
+          topic: 'memory_active_per_cent'
+        }
+      },
+      { // free available memory
+        condition: this.freeAvailableMemory,
+        result: {
+          payload: toMb(data.available - data.active) / (toMb(data.available) / 100), // free available excl. buffer / cache
+          topic: 'memory_free_available_per_cent'
+        }
+      },
+      { // total memory
+        condition: this.totalMemory,
+        result: {
+          payload: 100, // total
+          topic: 'memory_total_per_cent'
+        }
+      },
+      { // used memory
+        condition: this.usedMemory,
+        result: {
+          payload: toMb(data.used) / (toMb(data.total) / 100), // used
+          topic: 'memory_used_per_cent'
+        }
+      },
+      { // free memory
+        condition: this.freeMemory,
+        result: {
+          payload: toMb(data.free) / (toMb(data.total) / 100), // free
+          topic: 'memory_free_per_cent'
+        }
+      }
+    ]
+    for (let i = 0; i < possiblePayloads.length; i++) {
+      const possiblePayloadsItem = possiblePayloads[i]
+      if (possiblePayloadsItem.condition) {
+        payloadArr.push(possiblePayloadsItem.result)
+      }
     }
 
     return payloadArr
